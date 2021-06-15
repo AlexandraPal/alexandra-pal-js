26981abc43b58382cce8cb05be935be4ce3bf557
Alexandra Pal <68145080+AlexandraPal@users.noreply.github.com>
2021-06-15 21:47:13 +0300

    JS-Curs3

---
+++ M01S03/ex02/app.js
@@ -0,0 +1,63 @@
+var person = {
+  name: 'Alexandra',
+  surname: 'Pal',
+  age: 31,
+  petOwner: false,
+  skills: {
+    html: true,
+    css: true,
+    javaScript: true,
+  },
+  friends: {
+    larry: {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    steven: {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    carol: {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  },
+};
+
+console.log(person.friends.carol.age.toString());
+
+console.log((2021 - person.friends.larry.age).toString());
+
+console.log(
+  person.friends.steven.name +
+    ' este cu ' +
+    (person.friends.steven.age - person.friends.larry.age) +
+    ' an mai mare decat ' +
+    person.friends.larry.name +
+    '.',
+);
+
+console.log(
+  'Prietenul meu ' +
+    person.friends.steven.name +
+    ' ' +
+    person.friends.steven.surname +
+    ' are ' +
+    person.friends.steven.age +
+    ' ani.',
+);
+
+console.log(
+  'Prietenii mei sunt: ' +
+    person.friends.steven.name +
+    ', ' +
+    person.friends.carol.name +
+    ' si ' +
+    person.friends.larry.name +
+    '.',
+);
+
+console.log(person.friends.steven.surname.length.toString());
