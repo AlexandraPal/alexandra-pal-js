803ae63cc86e33f19fe9fa733c4c1d0ff4b6fc5c
Alexandra Pal <68145080+AlexandraPal@users.noreply.github.com>
2021-06-17 21:45:51 +0300

    JS1 - curs 4 final

---
+++ M01S03/ex05/app.js
@@ -0,0 +1,48 @@
+var person = {
+  name: 'Alexandra',
+  surname: 'Pal',
+  age: 31,
+  petOwner: false,
+  skills: ['html', 'css', 'javascript'],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
+
+console.log(
+  'Ma numesc ' +
+    person.name +
+    ' ' +
+    person.surname +
+    ' si stiu ' +
+    person.skills[0] +
+    ' si ' +
+    person.skills[1] +
+    '.',
+);
+
+console.log(
+  'Am ' +
+    person.friends.length +
+    ' prieteni: ' +
+    person.friends[0].name +
+    ', ' +
+    person.friends[1].name +
+    ' si ' +
+    person.friends[2].name +
+    '.',
+);
